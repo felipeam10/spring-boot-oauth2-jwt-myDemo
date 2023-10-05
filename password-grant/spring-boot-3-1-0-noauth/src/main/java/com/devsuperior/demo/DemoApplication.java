e1d9d184fab5b02ddd18b857fb51ec89c3a2d2d7
felipeam10 <felipeam10@hotmail.com>
2023-10-05 16:45:22 -0300

    v5.11 - BCrypt password encoder

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +12,29 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
+
+
+/*
+Validação interna que faz o spring security
+
+* @SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+
+		boolean result = passwordEncoder.matches("123456", "$2a$10$FxNVmbT/AvpKsRG1FNm2q.ZMxeIpMN/E/UeYM3jNhyNQlj8LjJfwC");
+		System.out.println("RESULTADO = " + result);
+	}
+}
+*
+* */
